9b0bf5808ab66dd671bbec7c3c78570c20508857
Gabriel Fernandes <fernandes.gh@outlook.com>
2022-07-08 17:40:39 -0300

    feat: butcher's quotes api

---
+++ server-api/butcher-quotes-server/app.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals 
+import random
+from flask import jsonify,request,Flask
+from flask_cors import CORS, cross_origin
+import pandas as pd
+
+BillyButcherApi = Flask(__name__)
+cors = CORS(BillyButcherApi)
+BillyButcherApi.config['CORS_HEADERS'] = 'Content-Type'
+
+df = pd.read_csv('finalQuotes.csv', names=['Speakers', 'Quotes'])
+
+def get_random_quote():
+    rq = random.randint(0, len(df))
+    speaking = df.values[rq][0]
+    quote = df.values[rq][1]
+    quote_obj = {"speaker": speaking, "quote": quote}
+    return quote_obj
+
+@cross_origin()
+@BillyButcherApi.route('/', methods=['GET'])
+def dashboard():
+    return jsonify(get_random_quote())
+
+if __name__ == '__main__':
+    BillyButcherApi.run(host= '127.0.0.1',port=5000)
---
+++ server-api/butcher-quotes-server/readme.md
@@ -0,0 +1,15 @@
+Billy Butcher Quotes Api
+====================
+Simple app to generate random Billy Butcher quotes using Python and Flask
+
+# Running locally
+With Python and Pip installed, install the dependencies with:
+```sh
+$ pip install -r requirements.txt
+```
+
+and finally run:
+```sh
+python3 app.py
+
+```
